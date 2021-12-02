61a79ac4ecab2ff2cfcdb69ed983f34d4dd5e0c0
Kalle (Sendou) <38327916+Sendouc@users.noreply.github.com>
2021-12-02 18:33:46 +0200

    Update template

--- template.ts
+++ template.ts
@@ -1,22 +1,28 @@
 import fs from "fs";
 import path from "path";
+import assert from "assert";
+
+const TEST_1_RESULT = -1;
+const TEST_2_RESULT = -1;
 
 function parseInput(input: string) {
-  return input.split("\n");
+  return input.trim().split("\n");
 }
 
 function main() {
-  const testInput = fs
-    .readFileSync(path.resolve(__dirname, "test-input.txt"), "utf8")
-    .trim();
-  const realInput = fs
-    .readFileSync(path.resolve(__dirname, "input.txt"), "utf8")
-    .trim();
+  const testInput = parseInput(
+    fs.readFileSync(path.resolve(__dirname, "test-input.txt"), "utf8")
+  );
+  const realInput = parseInput(
+    fs.readFileSync(path.resolve(__dirname, "input.txt"), "utf8")
+  );
 
   console.log("Test 1 is:");
+  // assert(-1 === TEST_1_RESULT);
   console.log("Exercise 1 is:");
 
   console.log("Test 2 is:");
+  // assert(-1 === TEST_2_RESULT);
   console.log("Exercise 2 is:");
 }
 
