53bc9ee647d37f4d2fe9dad696c74bc5637cbc2b
Kalle (Sendou) <38327916+Sendouc@users.noreply.github.com>
2021-12-07 18:15:33 +0200

    Update template

--- template.ts
+++ template.ts
@@ -9,6 +9,10 @@ function parseInput(input: string) {
   return input.trim().split("\n");
 }
 
+function solve(input: string[]): number {
+  return 0;
+}
+
 function main() {
   const testInput = parseInput(
     fs.readFileSync(path.resolve(__dirname, "test-input.txt"), "utf8")
@@ -17,9 +21,9 @@ function main() {
     fs.readFileSync(path.resolve(__dirname, "input.txt"), "utf8")
   );
 
-  console.log("Test 1 is:");
-  // assert(-1 === TEST_1_RESULT);
-  console.log("Exercise 1 is:");
+  console.log("Test 1 is:", solve(testInput));
+  assert(solve(testInput) === TEST_1_RESULT);
+  console.log("Exercise 1 is:", solve(realInput));
 
   console.log("Test 2 is:");
   // assert(-1 === TEST_2_RESULT);
